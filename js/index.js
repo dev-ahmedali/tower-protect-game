651fa7949336f93713621b68b2464a4f93bc0bd6
dev-ahmedali <ahmedali69325@gmail.com>
2022-12-26 16:47:01 +0600

    path finding

--- js/index.js
+++ js/index.js
@@ -8,7 +8,66 @@ ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
+
 image.onload = () => {
-  ctx.drawImage(image, 0, 0);
+  animate();
 };
 image.src = 'img/game-map.png';
+
+class Enemy {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position;
+    (this.width = 100), (this.height = 100), (this.wayPointsIndex = 0);
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2,
+    };
+  }
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    const wayPoint = wayPoints[this.wayPointsIndex];
+    const distanceY = wayPoint.y - this.center.y;
+    const distanceX = wayPoint.x - this.center.x;
+    const angle = Math.atan2(distanceY, distanceX);
+    this.position.x += Math.cos(angle);
+    this.position.y += Math.sin(angle);
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2,
+    };
+
+    if (
+      Math.round(this.center.x) === Math.round(wayPoint.x) &&
+      Math.round(this.center.y) === Math.round(wayPoint.y) &&
+      this.wayPointsIndex < wayPoints.length - 1
+    ) {
+      this.wayPointsIndex++;
+    }
+  }
+}
+
+const enemies = [];
+
+for (let i = 0; i < 10; i++) {
+  const xOffset = i * 150;
+  enemies.push(
+    new Enemy({
+      position: { x: wayPoints[0].x - xOffset, y: wayPoints[0].y },
+    })
+  );
+  console.log(wayPoints[0].x - xOffset);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.drawImage(image, 0, 0);
+  enemies.forEach((enemy) => {
+    enemy.update();
+  });
+}
